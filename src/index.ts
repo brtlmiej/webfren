934d91020210e1a1ebdfdf49cf86cc60649cf980
Bartłomiej Ciołek <50916407+brtlmiej@users.noreply.github.com>
2024-04-16 17:19:49 +0200

    chore(cli): Add version and name to Cli class (#23)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 /* v8 ignore start */
+import packageConfig from '../package.json' with { type: "json" };
 import { HttpLoadTestCommand } from "./cli/commands/HttpLoadTestCommand.js";
 import { LoadBalancerCommand } from "./cli/commands/LoadBalancerCommand.js";
 import { WebServerCommand } from "./cli/commands/WebServerCommand.js";
 import { Cli } from "./cli/Cli.js";
 
-const cli = new Cli();
+const cli = new Cli('webfren', packageConfig.version);
+
 cli.addCommand(HttpLoadTestCommand);
 cli.addCommand(LoadBalancerCommand);
 cli.addCommand(WebServerCommand);
+
 cli.register();
 cli.parse();
 /* v8 ignore stop */
